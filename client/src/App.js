2c4ded8baeb0a99c61b2e447688ad9f786cba737
Jonathan Andrews <jdotand@gmail.com>
2023-06-15 22:02:18 -0400

    media95 style

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,49 @@
-import { CssBaseline } from "@mui/material";
+import { CssBaseline, ThemeProvider, createTheme } from "@mui/material";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./scenes/home";
+
+const theme = createTheme({
+  typography: {
+    fontFamily: ["W95FA", "sans-serif"].join(","),
+    fontSize: 12,
+    h1: {
+      fontFamily: ["W95FA", "sans-serif"].join(","),
+      fontSize: 40,
+    },
+    h2: {
+      fontFamily: ["W95FA", "sans-serif"].join(","),
+      fontSize: 32,
+    },
+    h3: {
+      fontFamily: ["W95FA", "sans-serif"].join(","),
+      fontSize: 24,
+    },
+    h4: {
+      fontFamily: ["W95FA", "sans-serif"].join(","),
+      fontSize: 20,
+    },
+    h5: {
+      fontFamily: ["W95FA", "sans-serif"].join(","),
+      fontSize: 16,
+    },
+    h6: {
+      fontFamily: ["W95FA", "sans-serif"].join(","),
+      fontSize: 14,
+    },
+  },
+});
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        <CssBaseline>
-          <Routes>
-            <Route path="/" element={<Home />} />
-          </Routes>
-        </CssBaseline>
+        <ThemeProvider theme={theme}>
+          <CssBaseline>
+            <Routes>
+              <Route path="/" element={<Home />} />
+            </Routes>
+          </CssBaseline>
+        </ThemeProvider>
       </BrowserRouter>
     </div>
   );
--- client/src/scenes/home/index.jsx
+++ client/src/scenes/home/index.jsx
@@ -3,8 +3,8 @@ import {
   Box,
   Typography,
   Button,
-  TextField,
   useMediaQuery,
+  TextField,
 } from "@mui/material";
 
 const Home = () => {
@@ -12,27 +12,82 @@ const Home = () => {
 
   return (
     <Box>
-      <Typography mt="5rem" variant="h3" textAlign="center">
-        Youtube to MP3
+      <Typography mt="5rem" variant="h1" textAlign="center">
+        media95
       </Typography>
       <Box
-        p="2rem"
-        m="4rem auto"
-        id="input-submit"
-        width={isNonMobileScreen ? "60%" : "93%"}
+        className="box95-container"
+        sx={{
+          position: "fixed",
+          top: "40%",
+          left: "50%",
+          transform: "translate(-50%, -50%)",
+          width: "40%",
+          height: "35%",
+          backgroundColor: "#f3efe7",
+          color: "#383838",
+          borderLeft: "2px solid #ffffff",
+          borderTop: "2px solid #ffffff",
+          borderRight: "2px solid #686868",
+          borderBottom: "2px solid #686868",
+          borderRadius: "0px",
+          padding: "5px",
+          boxShadow: "3px 3px 0px black",
+          zIndex: "9999",
+        }}
       >
-        <TextField
-          id="url-input"
-          label="Youtube Video URL"
-          variant="filled"
-          defaultValue="" //"https://www.youtube.com/watch?v=zY64pu8dYi0&ab_channel=JAKED"
-          fullWidth
+        <Box
+          className="box95-header"
+          sx={{
+            backgroundColor: "#1d0d79",
+            color: "#ffffff",
+            width: "100%",
+            height: "10%",
+            paddingLeft: "1rem",
+            paddingTop: "0.35rem",
+            fontSize: "20px",
+          }}
         >
-          Youtube Video URL
-        </TextField>
-        <Box mt="1rem" textAlign="center">
-          <Button disableElevation type="submit" variant="contained">
-            Convert
+          media convert
+        </Box>
+        <Box id="url-input" mt="2rem" width="50%">
+          <Typography fontSize="30px">media url</Typography>
+          <TextField
+            variant="standard"
+            placeholder="https://www.youtube.com/watch?v=zY64pu8dYi0"
+            fullWidth
+            sx={{
+              textAlign: "center",
+              "& input:focus::placeholder": { color: "transparent" },
+            }}
+          >
+            Youtube Video URL
+          </TextField>
+        </Box>
+        <Box className="box95-button" m="1rem 0 0 1rem" textAlign="center">
+          <Button
+            disableElevation
+            size="large"
+            type="submit"
+            variant="contained"
+            sx={{
+              backgroundColor: "#F3EFE7",
+              borderLeft: "2px solid #ffffff",
+              borderTop: "2px solid #ffffff",
+              borderRight: "2px solid #686868",
+              borderBottom: "2px solid #686868",
+              textTransform: "lowercase",
+              borderRadius: "0",
+              "&:hover": {
+                backgroundColor: "#F3EFE7",
+                borderRadius: "0",
+                boxShadow: "3px 3px 0rem black",
+              },
+            }}
+          >
+            <Typography fontSize="20px" color="#383838">
+              convert
+            </Typography>
           </Button>
         </Box>
       </Box>
